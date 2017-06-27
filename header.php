a7960899639ba33365aa083ad147e2f1db2512fc
wellroam <449184990@qq.com>
2017-06-28 00:51:26 +0800

    wp

---
+++ header.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE HTML>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>大白鲨影视</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="dabaisha" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="wellroam" />
+
+
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <!-- Animate.css -->
+
+
+
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/magnific-popup.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="<?php bloginfo('template_url'); ?>/js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="<?php bloginfo('template_url'); ?>/js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
