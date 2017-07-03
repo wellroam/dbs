bc969a8d35e2f3f23da35e34d3384bae6e1a055c
wellroam <449184990@qq.com>
2017-07-04 00:39:29 +0800

    wp

---
+++ index1.php
@@ -0,0 +1,295 @@
+
+
+
+<?php get_header(); ?>
+
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2 text-left">
+					<div id="fh5co-logo"><a href="index.php">大白鲨影视<span>.</span></a></div>
+				</div>
+				<div class="col-xs-10 text-right menu-1">
+					<ul>
+						<li class="active"><a href="index.php">主页</a></li>
+						<li><a href="">作品</a></li>
+						<li><a href="">关于我们</a></li>
+						<li><a href="">团队经验</a></li>
+						<li><a href="">联系我们</a></li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/bg.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7 text-left">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeInUp">
+							<h1 class="mb30">每一帧都有灵魂</h1>
+							<p>（注：背景用魔兽动画gif图）</p>
+							<p>
+								<a href="#fh5co-project" target="_blank" class="btn btn-primary">了解我们</a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-project">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-8 col-md-offset-2 text-left fh5co-heading  animate-box">
+					<span>我们用镜头讲述你的故事</span>
+					<h2>作品</h2>
+					<p>大白鲨动画团队核心制作人员在影视广告、动画制作行业已经侵润十年有余，秉承以客户的成功作为我们的核心理念，力争为客户提供更多更具价值的服务</p>
+				</div>
+			</div>
+
+			<div class="row row-pb-md">
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail("img-responsive"); ?>
+						<div class="fh5co-copy">
+							<h3><?php the_title(); ?></h3>
+                            <p><?php the_excerpt(); ?></p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/work-2.jpg" alt="Free HTML5 Website Template by gettemplates.co" class="img-responsive">
+						<div class="fh5co-copy">
+							<h3>京东618</h3>
+							<p>导演:龚汉佳</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/work-3.jpg" alt="Free HTML5 Website Template by gettemplates.co" class="img-responsive">
+						<div class="fh5co-copy">
+							<h3>知法懂法宣传片</h3>
+							<p>导演：乔姐</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/work-1.jpg" alt="Free HTML5 Website Template by gettemplates.co" class="img-responsive">
+						<div class="fh5co-copy">
+							<h3>五粮液广告</h3>
+							<p>导演:孙杨</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+					<a href="#"><img src="images/work-2.jpg" alt="Free HTML5 Website Template by gettemplates.co" class="img-responsive">
+						<div class="fh5co-copy">
+							<h3>京东618</h3>
+							<p>导演:龚汉佳</p>
+						</div>
+					</a>
+				</div>
+
+			</div>
+
+
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-left fh5co-heading  animate-box">
+					<a href=""><span>查看更多.....</span> </a>
+
+				</div>
+			</div>
+		</div>
+		
+	</div>
+
+		<div id="fh5co-testimonial" class="fh5co-bg-section">
+			<div class="container">
+				<div class="row animate-box row-pb-md">
+					<div class="col-md-8 col-md-offset-2 text-left fh5co-heading">
+						<span>志同而气合</span>
+						<h2>伙伴怎么说</h2>
+						<p>发现并敢于尝试一个新事物确实非常困难，您可以先看看我们的用户怎么说。</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 animate-box">
+						<div class="testimonial">
+							<blockquote>
+								<p>&ldquo;非常好用的平台，用户体验做的很棒，有需求都能当天开发完成，开发速度简直惊人。&rdquo;</p>
+								<p class="author"> <img src="images/person1.jpg" alt="Free HTML5 Bootstrap Template by gettemplates.co"> <cite>&mdash; 吉比特《问道》游戏策划 江云</cite></p>
+							</blockquote>
+						</div>
+
+						<div class="testimonial fh5co-selected">
+							<blockquote>
+								<p>&ldquo;使用方便，上手简单！像超级市场，想要啥都有。平台服务积极主动，出现问题都能第一时间解决。而且手续费不高，比找外包公司实惠很多！&rdquo;</p>
+								<p class="author"><img src="images/person2.jpg" alt="Free HTML5 Bootstrap Template by gettemplates.co"> <cite>&mdash; 北京热锋网络 主策划</cite></p>
+							</blockquote>
+						</div>
+					</div>
+
+					<div class="col-md-6 animate-box">
+						<div class="testimonial fh5co-selected">
+							<blockquote>
+								<p>&ldquo;页面设计很直观，体感很NICE，期待更多合作。&rdquo;</p>
+								<p class="author"><img src="images/person3.jpg" alt="Free HTML5 Bootstrap Template by gettemplates.co"> <cite>&mdash; 西山居 约稿负责人</cite></p>
+							</blockquote>
+						</div>
+
+						<div class="testimonial">
+							<blockquote>
+								<p>&ldquo;真的很方便也安全。尤其短信提醒给点赞，不在电脑前也可以马上收到通知！&rdquo;</p>
+								<p class="author"><img src="images/person1.jpg" alt="Free HTML5 Bootstrap Template by gettemplates.co"> <cite>&mdash; 阳光地天主管经理</cite></p>
+							</blockquote>
+						</div>
+
+					</div>
+
+
+
+				</div>
+			</div>
+		</div>
+
+	<div id="fh5co-services" class="fh5co-bg-section border-bottom">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-8 col-md-offset-2 text-left animate-box" data-animate-effect="fadeInUp">
+					<div class="fh5co-heading">
+						<span>我们匠心独运用</span>
+						<h2>关于我们</h2>
+						<p>站在公司身后的，是一支年轻有为的实力团队。这里不单有制作经验丰富的导演，同时也有十余年经验的制作团队。</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-6 ">
+					<div class="feature-center animate-box" data-animate-effect="fadeInUp">
+						<span class="icon">
+							<i class="icon-eye"></i>
+						</span>
+						<h3>优秀团队</h3>
+						<p>多年从业经验的优秀团队，作品精美画风成熟。涵盖各种风格、不同领域，时刻准备应对您的不同需求。</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6 ">
+					<div class="feature-center animate-box" data-animate-effect="fadeInUp">
+						<span class="icon">
+							<i class="icon-command"></i>
+						</span>
+						<h3>节约成本</h3>
+						<p>没有转包及中介，由公司直接为您报价，保证每一分钱都体现在稿件质量上，用更低的价格获得更高质量的稿件。</p>
+
+					</div>
+				</div>
+				<div class="clearfix visible-sm-block"></div>
+				<div class="col-md-4 col-sm-6 ">
+					<div class="feature-center animate-box" data-animate-effect="fadeInUp">
+						<span class="icon">
+							<i class="icon-power"></i>
+						</span>
+						<h3>信誉保证</h3>
+						<p>完善的信誉体系，评价均来自真实交易，多项信誉数据供参考。全程担保交易，让您不再害怕跑单，约稿更安心。</p>
+
+					</div>
+				</div>
+
+				<div class="clearfix visible-md-block"></div>
+
+			</div>
+		</div>
+	</div>
+
+		<div id="fh5co-counter">
+			<div class="container">
+
+				<div class="row animate-box" data-animate-effect="fadeInUp">
+					<div class="col-md-8 col-md-offset-2 text-left fh5co-heading">
+						<span>不积硅步无以至千里</span>
+						<h2>团队经验</h2>
+						<p>介绍公司主要业务</p>
+					</div>
+				</div>
+
+				<div class="row">
+
+					<div class="col-md-3 col-md-offset-1 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+						<div class="feature-center">
+						<span class="icon">
+							<i class="ti-download"></i>
+						</span>
+							<span class="counter"><span class="js-counter" data-from="0" data-to="7" data-speed="15" data-refresh-interval="50">1</span>+</span>
+							<span class="counter-label">团队历史</span>
+
+						</div>
+					</div>
+					<div class="col-md-3 col-md-offset-2 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+						<div class="feature-center">
+							<span class="icon">
+								<i class="ti-face-smile"></i>
+							</span>
+							<span class="counter"><span class="js-counter" data-from="0" data-to="43" data-speed="15" data-refresh-interval="50">1</span>+</span>
+							<span class="counter-label">商业项目</span>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+		</div>
+
+
+
+	<div id="fh5co-started">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<span>联系我们</span>
+					<h2>联系我们</h2>
+					<p>这里放联系方式：电话，微博，微信，知乎……</p>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo">
+
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="<?php bloginfo('template_url'); ?>/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="<?php bloginfo('template_url'); ?>/js/jquery.waypoints.min.js"></script>
+	<!-- countTo -->
+	<script src="<?php bloginfo('template_url'); ?>/js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="<?php bloginfo('template_url'); ?>/js/jquery.magnific-popup.min.js"></script>
+	<script src="<?php bloginfo('template_url'); ?>/js/magnific-popup-options.js"></script>
+	<!-- Stellar -->
+	<script src="<?php bloginfo('template_url'); ?>/js/jquery.stellar.min.js"></script>
+	<!-- Main -->
+	<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+
+	</body>
+
+
